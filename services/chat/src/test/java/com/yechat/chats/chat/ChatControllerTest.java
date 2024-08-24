1398967f156627d1b20218fa6e41b6a6ad017271
Maks Balamut <balamutmaks@gmail.com>
2024-08-24 22:58:48 +0300

    test

--- services/chat/src/test/java/com/yechat/chats/chat/ChatControllerTest.java
+++ services/chat/src/test/java/com/yechat/chats/chat/ChatControllerTest.java
@@ -57,6 +57,7 @@ class ChatControllerTest {
         registry.add("spring.r2dbc.username", postgres::getUsername);
         registry.add("spring.r2dbc.password", postgres::getPassword);
         registry.add("spring.flyway.url", postgres::getJdbcUrl);
+
         registry.add("spring.flyway.user", postgres::getUsername);
         registry.add("spring.flyway.password", postgres::getPassword);
     }
