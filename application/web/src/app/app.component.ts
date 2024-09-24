e64100564d6bbe80dff10dfd3dec5e880e472769
Maks Balamut <balamutmaks@gmail.com>
2024-09-24 20:06:32 +0300

    Add text button

--- application/web/src/app/app.component.ts
+++ application/web/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FilledButtonComponent} from "./button/filled/filled-button.component";
 import {IconComponent} from "./icon/icon.component";
+import {TextButtonComponent} from "./button/text/text-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilledButtonComponent, IconComponent],
+  imports: [RouterOutlet, FilledButtonComponent, IconComponent, TextButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- application/web/src/app/button/text/text-button.component.ts
+++ application/web/src/app/button/text/text-button.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, ContentChildren, QueryList} from '@angular/core';
+import {ButtonComponent} from "../button.component";
+import {IconComponent} from "../../icon/icon.component";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-text-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './text-button.component.html',
   styleUrl: './text-button.component.scss'
 })
-export class TextButtonComponent {
+export class TextButtonComponent extends ButtonComponent {
 
+  @ContentChildren(IconComponent) icons!: QueryList<IconComponent>;
+
+  changePadding(): string[] {
+    if (!this.icons) return [];
+    return this.icons.length > 0 ? ['icon'] : [];
+  }
 }
